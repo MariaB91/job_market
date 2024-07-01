c578b6da7cffcbca2369e6616dba17f71c7968e1
Maria B <118015089+MariaB91@users.noreply.github.com>
2024-07-01 10:26:11 +0200

    Create Tests Unitaires.py

---
+++ Tests Unitaires.py
@@ -0,0 +1,10 @@
+# Tests unitaires avec pytest ou on peut utiliser Postman
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_read_jobs():
+    response = client.get("/jobs/")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
