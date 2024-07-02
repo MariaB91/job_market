a44787c3892b30ee04c684928c2be259e5ed1660
Maria B <118015089+MariaB91@users.noreply.github.com>
2024-07-02 10:01:21 +0000

    correction syntaxe

--- fastapi/api.py
+++ fastapi/api.py
@@ -1,5 +1,12 @@
 # Exemple de script pour interagir avec les APIs The Muse et Adzuna
 import requests
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/")
+async def read_root():
+    return {"message": "Hello World"}
 
 def fetch_jobs_from_themuse(api_url):
     response = requests.get(api_url)
--- mysql/init.sql
+++ mysql/init.sql
@@ -3,10 +3,12 @@
 --Table utilisateurs
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    city VARCHAR(50) NOT NULL
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    
 );
 
 --Table jobs
